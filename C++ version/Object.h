8ed5759f4b597a94ec13fc33d67d98b71ecf4529
JessinRa <jessin.bdg@gmail.com>
2018-04-16 10:35:34 +0700

    foldering

---
+++ C++ version/Object.h
@@ -0,0 +1,22 @@
+// asbtract class Object.h
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include "Lib/oop.hpp"
+
+using namespace std;
+
+class Object {
+	
+	public:	
+				
+		/*------------------------------------------------------------
+									Method  
+		------------------------------------------------------------*/
+		virtual void draw() = 0;	
+		
+	
+};
+
+#endif // !OBJECT_H
